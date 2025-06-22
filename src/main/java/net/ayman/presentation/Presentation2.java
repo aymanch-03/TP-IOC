a48e4ba506035a8e8f0fdd82dd94edcd0aa786a8
aymanch-03 <aymanechakar03@gmail.com>
2025-06-22 23:42:46 +0100

    use DaoImplV2

--- src/main/java/net/ayman/presentation/Presentation2.java
+++ src/main/java/net/ayman/presentation/Presentation2.java
@@ -1,18 +1,25 @@
 package net.ayman.presentation;
 
 import net.ayman.dao.IDao;
+import net.ayman.metier.IMetier;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Scanner;
 
 public class Presentation2 {
-    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Scanner scanner = new Scanner(new File("config.txt"));
 
         String daoClassName = scanner.nextLine();
         Class cDao = Class.forName(daoClassName);
         IDao dao=(IDao) cDao.newInstance();
-        System.out.println(dao.getData());
+
+        String metierClassName = scanner.nextLine();
+        Class cMetier = Class.forName(metierClassName);
+        IMetier metier=(IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
+
+        System.out.println("RES="+metier.calcul());
     }
 }
